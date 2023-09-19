ba587f5e84f9dbe3d57068ed552ef7f83128891a
Павел Филиппов <pgphil@yandex.ru>
2023-09-19 22:26:03 +0300

    Work on reviews.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key')
 
 DEBUG = os.getenv('DEBUG', default=False) == 'True'
-ALLOWED_HOSTS = ['158.160.67.222', '127.0.0.1', 'localhost', 'kitty.viewdns.net']
+ALLOWED_HOSTS = ['158.160.67.222', '127.0.0.1:9000', 'localhost', 'kitty.viewdns.net']
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1, localhost').split(', ')
 
 INSTALLED_APPS = [
