d95ab877b78a27c05cb8872ed0ff2442b78c2ba0
Павел Филиппов <pgphil@yandex.ru>
2023-09-20 09:44:45 +0300

    Try hide allowed_hosts to .env.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key')
 
 DEBUG = os.getenv('DEBUG', default=False) == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1:9000, localhost').split(', ')
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", default='127.0.0.1:9000, localhost').split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
