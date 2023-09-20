23dc16d9b4845b35934fcb8caf7a5faab634cdca
Павел Филиппов <pgphil@yandex.ru>
2023-09-20 14:27:03 +0300

    Correct after first review.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -14,7 +14,7 @@ DEBUG = os.getenv('DEBUG', default=False) == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1:9000, localhost').split()
 
 # ALLOWED_HOSTS = ['158.160.67.222', '127.0.0.1:9000', 'localhost', 'kitty.viewdns.net']
-
+#
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
